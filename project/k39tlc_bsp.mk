44faebc5444c7b5661bbfdbbb2f0e1eae6604ee2
melek saidani <MBCMELEK3@HOTMAIL.COM>
2024-07-30 16:25:29 -0700

    Update k39tlc_bsp.mk

--- project/k39tlc_bsp.mk
+++ project/k39tlc_bsp.mk
@@ -4,6 +4,10 @@ TARGET := k39tlc_bsp
 MODULES += app/mt_boot \
            dev/lcm
 PMIC_CHIP := MT6357
+ifeq ($(findstring PMIC_CHIP, $(strip $(DEFINES))),)
+DEFINES += PMIC_CHIP_$(shell echo $(PMIC_CHIP) | tr '[a-z]' '[A-Z]')
+endif
+MTK_UFS_SUPPORT = no
 MTK_EMMC_SUPPORT = yes
 DEFINES += MTK_NEW_COMBO_EMMC_SUPPORT
 MTK_KERNEL_POWER_OFF_CHARGING = yes
