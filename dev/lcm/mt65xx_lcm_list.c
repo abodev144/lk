ee4d75885f18851d40ff08684ace0459922869bc
melek saidani <MBCMELEK3@HOTMAIL.COM>
2024-07-30 11:54:11 -0700

    Update mt65xx_lcm_list.c

--- dev/lcm/mt65xx_lcm_list.c
+++ dev/lcm/mt65xx_lcm_list.c
@@ -52,13 +52,13 @@ extern LCM_DRIVER hct_otm1285a_dsi_vdo_hd_boe;
 extern LCM_DRIVER hct_ili9881_dsi_vdo_hd_cpt;
 extern LCM_DRIVER hct_hx8394f_dsi_vdo_hd_cmi;
 extern LCM_DRIVER hct_otm1282_dsi_vdo_hd_auo;
-extern LCM_DRIVER hct_rm68200_dsi_vdo_hd_cpt;
+extern LCM_DRIVER u3a_st7701_fwvga_dsi_ykl;
 extern LCM_DRIVER hct_nt35521s_dsi_vdo_hd_boe_50_xld;
 extern LCM_DRIVER hct_hx8394d_dsi_vdo_hd_cmi;
 
 LCM_DRIVER *lcm_driver_list[] = {
-#if defined(HCT_OTM1285A_DSI_VDO_HD_BOE)
-       &hct_otm1285a_dsi_vdo_hd_boe,
+#if defined(U3A_ST7701_FWVGA_DSI_YKL)
+       &u3a_st7701_fwvga_dsi_ykl,
 #endif
 
 #if defined(HCT_ILI9881_DSI_VDO_HD_CPT)
