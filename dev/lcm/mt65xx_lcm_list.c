19e912c2a3625da2b155feb357353c61fc1f3eeb
melek saidani <MBCMELEK3@HOTMAIL.COM>
2024-07-31 06:32:34 -0700

    Update mt65xx_lcm_list.c

--- dev/lcm/mt65xx_lcm_list.c
+++ dev/lcm/mt65xx_lcm_list.c
@@ -58,7 +58,7 @@ extern LCM_DRIVER hct_hx8394d_dsi_vdo_hd_cmi;
 
 LCM_DRIVER *lcm_driver_list[] = {
 #if defined(U3A_ST7701_FWVGA_DSI_YKL)
-       &u3a_st7701_fwvga_dsi_ykl_lcm_drv,
+       &u3a_st7701_fwvga_dsi_ykl,
 #endif
 
 #if defined(HCT_ILI9881_DSI_VDO_HD_CPT)
